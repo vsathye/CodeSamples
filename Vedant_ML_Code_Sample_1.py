cc8db9a95a3af64ba34bb8c367c154e1d4a39581
vsathye <49597649+vsathye@users.noreply.github.com>
2021-01-06 23:01:52 -0800

    Few different Code Samples

---
+++ Vedant_ML_Code_Sample_1.py
@@ -0,0 +1,85 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+"""Example script that uses ML to break large paragraphs of text into keywords which are then used for sentiment analysis"""
+
+import pandas as pd
+import numpy as np
+import os
+from rake_nltk import Metric, Rake
+from pathlib import Path
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.linear_model import LogisticRegression
+import spacy
+from spacy.lang.en.stop_words import STOP_WORDS
+from spacy.lang.en import English
+from sklearn.feature_extraction.text import CountVectorizer,TfidfVectorizer
+from sklearn.base import TransformerMixin
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+import matplotlib.pyplot as plt
+
+path = "Path"
+data_folder = Path(path)
+fileto = data_folder/"SOMEFILE"
+df = pd.read_csv(fileto)
+df = df.replace("#REF!",None)
+df.dropna(inplace = True)
+#load a dictionary
+def load_words():
+    fileto = data_folder/"words_alpha.txt"
+    with open(fileto) as word_file:
+        valid_words = set(word_file.read().split())
+    return valid_words
+english_words = load_words()
+
+#scale linear features
+for i in range(5):
+    df["Label" + str(i)] = [int(100*number) for number in list(df["Label" + str(i)])]
+    
+#load spacy
+punctuations = string.punctuation
+nlp = spacy.load('en')
+stop_words = spacy.lang.en.stop_words.STOP_WORDS
+parser = English()
+
+def tokenizer_(phrase):
+    tokens = parser(phrase)
+    tokens = [ word.lemma_.lower().strip() for word in tokens ]
+    tokens = [ word for word in tokens if word not in stop_words and word not in punctuations ]
+    return mytokens
+class predictors(TransformerMixin):
+    def transform(self, X, **transform_params):
+        return [text.strip().lower() for text in X]
+    def fit(self, X, y=None, **fit_params):
+        return self
+    
+bow_vector = CountVectorizer(tokenizer = tokenizer_, ngram_range=(1,1))
+
+#figures out the accuracy score for a basic logistic regression model on data
+for a in df.columns:
+    X = df['Label'] 
+    ylabels = df[str(a)] 
+    X_train, X_test, y_train, y_test = train_test_split(X, ylabels, test_size=0.3)
+    classifier = LogisticRegression()
+    pipe = Pipeline([("cleaner", predictors()),('vectorizer', bow_vector),('classifier', classifier)])
+    pipe.fit(X_train,y_train)
+    predicted = pipe.predict(X_test)
+    corr = 0
+    tot = 0
+    y_test2 = y_test.reset_index().drop(columns = ["index"])
+    y_test2 = pd.Series(y_test2[str(a)])
+    for i,v in y_test2.items():
+        if(abs(y_test2[i] - predicted[i]) <= 50):
+            corr+=1
+        tot +=1
+    print("Model Accuracy:",corr/tot)
+    print("Model Precision:",metrics.precision_score(y_test, predicted, average = 'micro'))
+    print("Model Recall:",metrics.recall_score(y_test, predicted, average = 'micro'))
+    print()
+
---
+++ shuf.py
@@ -0,0 +1,125 @@
+#!/usr/bin/python
+
+import random, sys
+from optparse import OptionParser
+
+def get_lines(filename):
+    f = open(filename, 'r')
+    lines = [x for x in f]
+    f.close()
+    return lines
+
+class randline:
+    def __init__(self, filename, uselines = False, posmin = 0, posmax = float('inf'), use_bounds = False):
+        self.lines = []
+        self.mymin = 0
+        self.mymax = float('inf')
+        if use_bounds:
+            self.mymin = posmin
+            self.mymax = posmax
+        elif uselines:
+            self.lines = [x for x in sys.stdin]
+        else:
+            self.lines = get_lines(filename)
+        if use_bounds:
+            self.lines = []
+            for i in range(self.mymin, self.mymax):
+                self.lines.append(str(i) + "\n")
+            
+
+    def chooseline(self):
+        return random.choice(self.lines)
+
+    def get_n_random_lines(self, numlines):
+        numlines = min(numlines, len(self.lines))
+        return random.sample(self.lines,numlines)
+
+    def choose_no_repeat(self):
+        return random.choice(self.lines)
+
+
+
+def main():
+    minr = 0
+    maxr = float('inf')
+    use_bounds = False
+    mycount = -1
+    input_file = ""
+    num_list = []
+    version_msg = "%prog 2.0"
+    usage_msg = """%prog [OPTION]... FILE Output randomly selected lines from FILE. Use -r to allow repeats, -n to spcify a number of lines, and -i for a range of lines to read from. If no file name is given or a '-' is given file to be read will be stdin"""
+
+    parser = OptionParser(version=version_msg,
+                          usage=usage_msg)
+    parser.add_option("-n", "--head-count",
+                      action="store", dest="headcount", default=None,
+                      help="output at most COUNT lines")
+    parser.add_option("-i", "--input-range ",
+                      action="store", dest="inputrange", default=None,
+                      help=" treat each number LO through HI as an input line")
+    parser.add_option("-r", "--repeat",
+                      action="store_true", dest="repeat", default=False,
+                      help="output lines can be repeated")
+
+    options, args = parser.parse_args(sys.argv[1:])
+
+    if options.headcount:
+        try:
+            mycount = int(options.headcount)
+                
+        except :
+            parser.error("Bad line count"+ str(options.headcount))
+
+        if mycount <0:
+            parser.error("Negative line count")
+
+    if options.inputrange:
+        if len(args) != 0:
+            parser.error("extra operand")
+        try:
+            find_dash = str(options.inputrange).index('-')
+        except:
+            parser.error("Bas input range")
+        try:
+            minr = int(str(options.inputrange).split("-")[0])
+            maxr = int(str(options.inputrange).split("-")[1])+1
+            use_bounds = True
+        except:
+            parser.error("Bad input range")
+        if minr<0 or maxr<0:
+            parser.error("negative range")
+
+    if options.inputrange and minr >= maxr:
+        parser.error("Bad input range, min greater than max")
+
+
+    read_in = False
+    if len(args) == 0 or args[0] == '-':
+        read_in = True 
+    if len(args) >1:
+        parser.error("Too many args")
+    if not read_in:
+        input_file = args[0]
+
+    try:
+        generator = randline(filename = input_file, uselines = read_in, posmin = minr, posmax = maxr, use_bounds = use_bounds)
+        if(bool(options.repeat)):
+            if mycount == -1:
+                while True:
+                    sys.stdout.write(generator.chooseline())
+            else:
+                for i in range(mycount):
+                    sys.stdout.write(generator.chooseline())
+        else:
+            if mycount == -1:
+                for i in generator.get_n_random_lines(float('inf')):
+                    sys.stdout.write(i)
+            else:
+                for i in generator.get_n_random_lines(mycount):
+                    sys.stdout.write(i)
+    except IOError as ioe:
+        parser.error("I/O error({0}): {1}".
+                     format(errno, strerror))
+
+if __name__ == "__main__":
+    main()
